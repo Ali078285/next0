c4451aeb4105a3b6d433d5638a5ab0999ec8149c
Ali078285 <118333909+Ali078285@users.noreply.github.com>
2022-11-16 10:12:01 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>Ali Next</main>
+      <main className={styles.main}>
+        <h1>Ali Next</h1>
+      </main>
 
       <footer className={styles.footer}>
         <a
